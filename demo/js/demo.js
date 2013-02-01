90e5943e2dfb5f7001ed5ae404a9f799ea67678e
ford_lee <fordlee404@gmail.com>
2013-02-01 14:32:42 +0800

    update demo folder tree

--- demo/js/demo.js
+++ demo/js/demo.js
@@ -1,16 +1,16 @@
 $(function(){
-    var $imgRadio = $('input[name="img"]'),
-        img = document.getElementById('img_show_tag'),
-        _val, _src;
+    var $imgRadio = $('input[name="img"]');
 
     $imgRadio.bind('click', function(){
-        img.src = '';
-        _val = this.value;
+        var img = document.getElementById('img_show_tag'),
+            _src;
 
-        imgPreloading(_val, function(_img){
+        img.src = '';
+        _src = this.value;
+
+        imgPreloading(_src, function(_img){
             img.width = _img.width;
             img.height = _img.height;
-            _src = _val;
             setTimeout(function(){
                 img.src = _src;
             }, 1000);
